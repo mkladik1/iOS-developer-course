38fd0ffd165b665eeb73a05e18f810300be5ab81
mkladik1 <mkladik1@gmail.com>
2016-02-09 17:34:36 +0100

    Lesson2 second commit

--- Lesson2/tecaj.playground/Contents.swift
+++ Lesson2/tecaj.playground/Contents.swift
@@ -3,4 +3,39 @@
 import UIKit
 import Foundation
 
+class Money{
+    var currency: String = ""
+}
 
+
+
+
+class MoneyConverter{
+    func convert(var value: Float, startCurrency: Money, targetCurrency: Money) -> (convertedValue: Float, targetCurrency: Money) {
+        if (startCurrency.currency == "EUR") && (targetCurrency.currency == "USD")  {
+            value = value * 1.12
+        }
+        
+        else if (startCurrency.currency == "USD") && (targetCurrency.currency == "EUR")  {
+            value = value * 0.89
+        }
+        else {
+            
+        }
+        
+        return (value, targetCurrency)
+    }
+}
+
+let dollar = Money()
+dollar.currency = "USD"
+
+let euro = Money()
+euro.currency = "EUR"
+
+let sit = Money()
+sit.currency = "SIT"
+
+let dollarToEuro = MoneyConverter().convert(10, startCurrency: dollar, targetCurrency: euro)
+let euroToDollar = MoneyConverter().convert(100, startCurrency: euro, targetCurrency: dollar)
+let euroToSit = MoneyConverter().convert(1, startCurrency: euro, targetCurrency: sit)
