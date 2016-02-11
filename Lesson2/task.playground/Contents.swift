a2fd7f71b9422103307c378691607de74e04b853
mkladik1 <mkladik1@gmail.com>
2016-02-11 08:29:46 +0100

    Lesson2 commit toDoTask payground

--- Lesson2/task.playground/Contents.swift
+++ Lesson2/task.playground/Contents.swift
@@ -1,5 +1,41 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation;
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
+
+class toDoTask
+{
+    var taskName: String = ""
+    var dateAdd: NSDate?
+    var dateChange: NSDate?
+    var stateStr: String = ""
+    enum state: String
+        {
+        case NotDone = "Not done"
+        , Done = "Done"
+        }
+    
+    var desc : String
+        {
+        return "Description"
+        }
+    
+    init(taskName: String)
+        {
+        self.taskName = taskName
+        self.stateStr = state.NotDone.rawValue
+        }
+}
+
+
+let task1 = toDoTask(taskName: "check backup")
+task1.taskName
+task1.desc
+task1.stateStr
+
+let task2 = toDoTask(taskName: "migrate database")
+task2.taskName
+task2.stateStr = toDoTask.state.Done.rawValue
+task2.stateStr
